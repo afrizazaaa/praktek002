4ba46f1f822b2a77236288a9f9271392d00b1ff8
afriza <afrizahelmi35@gmail.com>
2018-03-29 16:12:14 +0800

    membuat atribut dan behavior

--- src/praktek002/karpet.java
+++ src/praktek002/karpet.java
@@ -1,4 +1,12 @@
 package praktek002;
 public class karpet {
+    double panjang;
+    double lebar;
     
+    void cetakInfo() {
+        System.out.println("=======================");
+        System.out.println("Panjang :"+panjang);
+        System.out.println("Lebar   :"+lebar);
+        System.out.println("=======================");
+    }
 }
